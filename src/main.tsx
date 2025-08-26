1b7f3fd0e455df1fd9358f3cc090936df4b266c1
Yulia Podgurskaia <linguavoice1@yahoo.com>
2025-08-26 23:13:29 +0300

    feat: add ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './global.scss';
 import App from './App/App.tsx';
+import ErrorBoundary from './ui/ErrorBoundary/ErrorBoundary.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element not found');
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
